b26f86281a8b3d01e0e76de4ba85ac15afcaf0ed
hanlei840405 <hanlei.840405@gmail.com>
2016-10-09 23:14:08 +0800

    init

---
+++ src/main/java/com/fruit/PitayaApplication.java
@@ -0,0 +1,12 @@
+package com.fruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PitayaApplication.class, args);
+	}
+}
