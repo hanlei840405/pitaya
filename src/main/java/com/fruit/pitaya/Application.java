1f7bd818bb9a009e787bcc885260fa334e58e64b
360BUYAD\hanlei6 <hanlei.840405@gmail.com>
2017-06-07 16:30:13 +0800

    修复文件上传临时目录问题

--- src/main/java/com/fruit/pitaya/Application.java
+++ src/main/java/com/fruit/pitaya/Application.java
@@ -2,9 +2,28 @@ package com.fruit.pitaya;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.MultipartConfigElement;
+import java.io.File;
 
 @SpringBootApplication
 public class Application {
+    /**
+     * 文件上传临时路径
+     */
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        String tmpPath = System.getProperties().getProperty("user.home") + "/tmp/";
+        File file = new File(tmpPath);
+        if (!file.exists()) {
+            file.mkdir();
+        }
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setLocation(System.getProperties().getProperty("user.home") + "/tmp/");
+        return factory.createMultipartConfig();
+    }
 
     public static void main(String[] args) {
 //		Md5PasswordEncoder encoder = new Md5PasswordEncoder();
